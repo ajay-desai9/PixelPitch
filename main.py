3a2cd498a7de0ceb6eb1ec00163815f1c4af2b18
ajay-desai9 <ajay.desai509@gmail.com>
2025-06-10 16:47:13 -0400

    player team assignment logic

--- main.py
+++ main.py
@@ -5,19 +5,46 @@ def main():
     
     video_frames = read_video("input_videos/08fd33_4.mp4")
 
+    #Player tracking
     tracker = Tracker("models/best.pt")
 
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path="stubs/track_stubs.pkl")
-    
+    #Get object positions
     tracker.add_position_to_tracks(tracks)
 
+    #Player team assignment
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
     
-    ## Draw object Tracks
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+    
+    ## Draw object racks
     output_video_frames = tracker.draw_annotations(video_frames, tracks,team_ball_control)
 
     save_video(output_video_frames, "output_videos/output_video.mp4")
+
+    #Player team assignment
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
 
 if __name__ == '__main__':
